919e5407ec231d191c2cdf2c8ccf495312eba0cf
Abdur Rahman <khalil.ahmad1547@gmail.com>
2020-04-03 10:55:43 +0500

    A Basic template for Project
    A basic template for Project has Created with important function But there are functions to implement till nw

--- Client/Client.py
+++ Client/Client.py
@@ -2,20 +2,26 @@
 # importing libraries
 import socket
 import sys
-
+import json
 
 class Client:
-    MyName = ''
-    Socket = ''
-    OtherClient = ''
-    OnlineClients = ''
+    MyName = None     # My user name
+    MyId = None   # My unique Id stored in server
+    MyGroups = {}   # Groups which I have joined
+    # MyGroups = {'Group Name':group_Id}
+    MyContacts = {}     # List of contacts which i have saved
+    # MyContacts = {Contact_Name:His_ID}
+    MyStatus = False   # I am online or offline
+    Socket = None     # socket
+    OtherClient = None        # to which client of group i am chatting
+    OnlineClients = None      # other online clients
 
     def SendMessage(self,):
         print(self.OnlineClients)
         self.OtherClient = input("Enter user to connect :")
         msg = input(">>> ")
         msg = self.EncodeMessage(msg)
-        Client.Socket.sendall(msg)
+        self.Socket.sendall(msg)
         return msg.decode('UTF-8')
 
     def RecvMessage(self):
@@ -27,54 +33,114 @@ class Client:
         msg = msg.encode('UTF-8')
         return msg
 
-    def regester(self):
-        Client.Socket.sendall(Client.MyName.encode('UTF-8'))
-        Client.OnlineClients = Client.Socket.recv(1024)
-        Client.OnlineClients = Client.OnlineClients.decode('UTF-8')
+    def SignUp(self):   # will do sign Up
+        self.MyName = input("Enter your User Name :")
+        temp = input("Enter Your Password :")
+        temp = "r<up<"+ temp
+        self.Socket.sendall(temp.encode('UTF-8'))
+        self.MyId = self.Socket.recv(1024).decode('UTF-8')
+        print("Sign Up Successfully")
+        print("Your user name :", self.MyName)
+        print("Your ID is :", self.MyId)
 
+    def SignIn(self):   # will do sign in
+        if self.MyName is None:
+            self.MyName = input("Enter User Name :")
+        if self.MyId is None:
+            self.MyId = input("Enter Your ID :")
+        temp = input("Enter Your Password :")
+        temp = "r<in<"+str(self.MyId)+"<"+str(temp)
+        self.Socket.sendall(temp.encode('UTF-8'))
+        temp = self.Socket.recv(1024)
+        if temp.decode('UTF-8') == 'True':
+            print("Sign in successful")
+            self.MyStatus = True
+        else:
+            print("Try again")
+            self.SignIn()
+
+    def CreateGroup(self):  # will create a New group
+        pass
+
+    def GoOffline(self):    # will disconnect from server
+        pass
+
+    def AddContact(self):   # will add new contact
+        pass
+
+    def MyProfile(self):    # will print My basics info
+        pass
+
+    def Chat(self):
+        pass
 
     def close(self):
         try:
             print("closing socket")
-            Client.Socket.close()
+            self.Socket.close()
         except socket.error as err:
             print("socket closing error :",err)
             sys.exit("Socket closing error")
 
-
-    def __init__(self):
+    def ConnectToServer(self):
         try:
-            Client.MyName = input("Enter user name :")
+            # self.MyName = input("Enter user name :")
             print("Creating Socket")
-            Client.Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         except socket.error as err:
             print("Socket creating error :", err)
             sys.exit("Socket creating error ")
-
         ServerIP = 'localhost'
         ServerPort = 8080
         ServerAdress = (ServerIP, ServerPort)
-
-
         try:
             print("connecting to server :", ServerAdress)
-            Client.Socket.connect(ServerAdress)
+            self.Socket.connect(ServerAdress)
         except socket.error as err:
             print("error in connecting to server :", err)
             sys.exit("error in connecting to server :")
         else:
             print("connected to the server")
-        self.regester()
-        '''msg = ''
-                while msg != '\end':
-                    msg = self.SendMessage(self.Socket)
-                self.Socket.close()'''
+
+    def Decoder(self):
+        while True:
+            if self.Socket is None:     # not connected to server
+                print("1. Go Online")
+                print("2. Exit")
+                temp = input(">>>")
+                if temp is '1':
+                    self.ConnectToServer()
+                elif temp is '2':
+                    break
+            elif self.MyStatus is False:     # I have't Sign in
+                print("1. Sign UP")
+                print("2. Sign In")
+                temp = input(">>>")
+                if temp is '1':
+                    self.SignUp()
+                elif temp is '2':
+                    self.SignIn()
+            else:
+                print("1. Create New Group")
+                print("2. Chat")
+                print("3. Go Offline")
+                print("4. Exit")
+                temp = input(">>>")
+                if temp is '1':
+                    self.CreateGroup()
+                elif temp is '2':
+                    self.Chat()
+                elif temp is '3':
+                    self.GoOffline()
+                elif temp is '4':
+                    self.Socket.close()
+                    break
+                else:
+                    print("Please Enter a Valid Option")
+
+    def __init__(self):
+        self.Decoder()
 
 
 if __name__ == '__main__':
     MyClient = Client()
-    msg = ''
-    while msg!='\end':
-        msg = MyClient.SendMessage()
-        MyClient.RecvMessage()
-    MyClient.close()
--- Server/Server.py
+++ Server/Server.py
@@ -3,46 +3,99 @@
 import socket
 import sys
 import threading
-
+import json
+import random
 
 class Server:
-    # total clients to be handled at a time
-    MaxClient = 5
-    # available clients
-    OnlineClients = {}
+    MaxClient = 5   # total clients to be handled at a time
+    # OnlineClients = {}  # available clients
+    Groups = {}     # Groups Name with there IDs
+    # Groups = {GroupID:[Group Members List]}
+    Admins = {}     # Each Group's Admin ID and Group's ID
+    # Admins = {GroupID:Group_Admin_ID}
+    Clients = {}    # Each Clients ID and Socket(if available)
+    # Clients = {Clients_ID:Client_Socket}
+    CurrentClientStatus = {}    # Each Client's ID with its Sign In status
+    # CurrentClientStatus = {ClientID:'True/False'}
+    ClientPass = {}     # Each Client's ID with its Password
+    # ClientPass = {Client_ID:Password}
+    Buffer = {}     # to store un sended data with id
+    # Buffer = {ID:Message}
 
-    def DecodeMessage(self, msg):
-        msg = msg.decode('UTF-8')
+    def SignIn(self, id, password, sock):
+        print("Sign in request form ", sock)
+        if str(id) in str(self.ClientPass.keys()):
+            print("ID found ...")
+            if str(self.ClientPass[id]) == str(password):
+                print("Password matched ...")
+                rep = 'True'
+                sock.sendall(rep.encode('UTF-8'))
+                print(self.ClientPass)
+            else:
+                print("Password not matched ...")
+                rep = 'False'
+                sock.sendall(rep.encode('UTF-8'))
+        else:
+            print("ID not present ...")
+            rep = 'False'
+            sock.sendall(rep.encode('UTF-8'))
+
+    def SignUp(self, password, sock):
+        # print("New Sign Up request ...")
+        print("Generating Random key ...")
+        temp = random.randint(0, 10000)
+        temp = str(temp)
+        if temp not in self.ClientPass.keys():
+            print("Adding to Client's list ...")
+            self.ClientPass[temp] = str(password)
+            print(self.ClientPass)
+            print("Added Successfully ...")
+            print("Sending key to Client ...")
+            sock.sendall(temp.encode('UTF-8'))
+            print("Sended Successfully ...")
+        else:
+            print("Key is present already ...")
+            self.SignUp(password)
+
+    def CreateGroup(self):
+        pass
+
+    def Decoder(self, msg, sock):
         msg = msg.split("<")
-        return msg
+        print(msg)
+        if msg[0] == 'r':
+            if msg[1] == 'in':
+                try:
+                    self.SignIn(msg[2], msg[3], sock)
+                except IndexError as err:
+                    print("Error in msg ...")
+                    print(err)
+                    rep = 'False'
+                    sock.sendall(rep.encode('UTF-8'))
+                else:
+                    pass
+            if msg[1] == 'up':
+                try:
+                    self.SignUp(msg[2], sock)
+                except IndexError as err:
+                    print("ERROR in message ...")
+
 
     def Handler(self, sock, adr):
-        # print("Got A connection from :",adr)
-        # self.ClientName = ''
-        # self.ClientName = "Please tell us you name :"
-        ClientName = sock.recv(1024)
-        ClientName = ClientName.decode('UTF-8')
-        print(ClientName)
-        Server.OnlineClients[ClientName] = sock
-        print("client ",ClientName," added to lsit")
-        print(self.OnlineClients)
-        sock.sendall(str(Server.OnlineClients.keys()).encode('UTF-8'))
         while True:
-            msg = self.DecodeMessage(sock.recv(1024))
-            print(msg)
-            Server.OnlineClients[str(msg[0])].sendall(str(msg[1]).encode('UTF-8'))
-            if not msg[1]:
-                del Server.OnlineClients[ClientName]
-                print(Server.OnlineClients)
+            msg = sock.recv(1024)
+            msg = msg.decode('UTF-8')
+            if not msg:
                 break
             else:
-                print("Message from :", adr, msg[1], " to ", Server.OnlineClients[str(msg[0])])
+                self.Decoder(msg, sock)
+
 
 
     def __init__(self):
         try:
-            print("Creating Socket")
-            Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print("Creating Socket ...")
+            self.Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         except socket.error as err:
             print("Socket creating error :", err)
             sys.exit("Socket creating error ")
@@ -51,16 +104,16 @@ class Server:
         ServerAdress = (ServerIP, ServerPort)
         try:
             print("Running Server at address :", ServerAdress)
-            Socket.bind(ServerAdress)
+            self.Socket.bind(ServerAdress)
         except socket.error as err:
             print("Socket Binding error :", err)
         else:
             print("Server Running Successfully")
 
-        Socket.listen(Server.MaxClient)
+        self.Socket.listen(self.MaxClient)
         while True:
             print("Waiting for connections")
-            ClientSocket, ClientAdress = Socket.accept()
+            ClientSocket, ClientAdress = self.Socket.accept()
             print("got a connection from ", ClientAdress)
             newClient = threading.Thread(target=self.Handler, args=(ClientSocket, ClientAdress))
             newClient.start()
