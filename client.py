08fee58d4195015097e69b5a2580ba870d3c068a
Abdullah296 <60434057+Abdullah296@users.noreply.github.com>
2020-06-09 21:26:14 +0500

    Multi threading and filesharing
    helps in bigger code

--- client.py
+++ client.py
@@ -7,11 +7,50 @@ Created on Tue Jun  9 12:01:23 2020
 
 import socket as S
 import threading as T
+import tqdm
+import os
 
 def Receive(client):
     while True:
         msg = client.recv(1024)
-        print(msg.decode("utf-8"))
+       # if(msg == 'file'):
+        #    recievefile(client)
+        else:
+            print(msg.decode("utf-8"))
+'''
+def recievefile(ClientSocket):
+    BUFFER_SIZE = 4096
+    received = ClientSocket.recv(BUFFER_SIZE).decode()
+    filename, filesize = received.split()
+    filename = os.path.basename(filename)
+    filesize = int(filesize)
+    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    print('hello')
+    with open(filename, "wb") as f:
+        for _ in progress:
+            bytes_read = ClientSocket.recv(BUFFER_SIZE)
+            if not bytes_read:    
+                break
+            f.write(bytes_read)
+            progress.update(len(bytes_read))
+ '''           
+def sendfile():
+    BUFFER_SIZE = 4096
+    filename = input("Enter file path: ")
+    filesize = os.path.getsize(filename)
+     #s = socket.socket()
+        #print(f"[+] Connecting to {host}:{port}")
+        #s.connect((host, port))
+        #print("[+] Connected.")
+    client_socket.send(f"{filename} {filesize}".encode())
+    progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "rb") as f:
+        for _ in progress:
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                break
+            client_socket.sendall(bytes_read)
+            progress.update(len(bytes_read))
         
 client_socket = S.socket(S.AF_INET, S.SOCK_STREAM)
 host = 'localhost'
@@ -23,11 +62,21 @@ thread.start()
 
 msg = input("Enter name: ")
 client_socket.send(msg.encode("utf-8"))
+
+
     
 while True:
+    a = input("Enter option \n1. send file \n2. send message\n")
+    print(a)
+    if a is 2:
+        print("hello dude")
+        msg = 'file'
+        client_socket.send(msg.encode("utf-8"))
+        sendfile()
     
-    msg = input("You:")
-
-    client_socket.send(msg.encode("utf-8"))
-    
+    else:    
+        msg = input("You:")
+        client_socket.send(msg.encode("utf-8"))          
+            
 client_socket.close()
+
--- server.py
+++ server.py
@@ -7,6 +7,8 @@ Created on Tue Jun  9 11:50:10 2020
 
 import socket as S
 import threading as T
+import tqdm
+import os
 
 Clients = []
 Client = {}
@@ -18,8 +20,48 @@ def handler(client_socket,client_address):
         if not data:
             break
         else:
-             data =  " " + str(Client[client_address[1]]) + " --> " + data.decode("utf-8")
-             SendMsg(client_socket , data)
+             if data.decode("utf-8") == 'file':
+                 recievefile(client_socket)
+             else:
+                 data =  " " + str(Client[client_address[1]]) + " --> " + data.decode("utf-8")
+                 SendMsg(client_socket , data)
+def recievefile(ClientSocket):
+    #print('hello1')
+    BUFFER_SIZE = 4096
+    received = ClientSocket.recv(BUFFER_SIZE).decode()
+    filename, filesize = received.split()
+    filename = os.path.basename(filename)
+    filesize = int(filesize)
+    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    print('hello')
+    with open(filename, "wb") as f:
+        for _ in progress:
+            bytes_read = ClientSocket.recv(BUFFER_SIZE)
+            if not bytes_read:    
+                break
+            f.write(bytes_read)
+            progress.update(len(bytes_read))
+    '''        
+    sendfile(ClientSocket, filename)
+
+def sendfile(clientS, filename):
+    for client in Clients:
+        if client != clientS:
+            msg = 'file'
+            client.send(msg.encode("utf-8"))
+            BUFFER_SIZE = 4096   
+            filesize = os.path.getsize(filename)
+     
+            client.send(f"{filename} {filesize}".encode())
+            progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+            with open(filename, "rb") as f:
+                for _ in progress:
+                    bytes_read = f.read(BUFFER_SIZE)
+                    if not bytes_read:
+                        break
+                    client.sendall(bytes_read)
+                    progress.update(len(bytes_read))
+           '''         
 def SendMsg(clientS,msg):
     for client in Clients:
         if client != clientS:
@@ -43,3 +85,25 @@ while True:
     
     new_thread = T.Thread(target=handler,args=(client_S,client_A))
     new_thread.start()
+    
+    
+    
+    
+    
+'''
+BUFFER_SIZE = 4096
+            received = ClientSocket.recv(BUFFER_SIZE).decode()
+            filename, filesize = received.split()
+            filename = os.path.basename(filename)
+            filesize = int(filesize)
+            progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+            with open(filename, "wb") as f:
+                for _ in progress:
+                    bytes_read = ClientSocket.recv(BUFFER_SIZE)
+                    if not bytes_read:    
+                        break
+                    f.write(bytes_read)
+                    progress.update(len(bytes_read))
+            
+            ClientSocket.close()
+'''
